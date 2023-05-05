8f646b3ba8920b5b2c9218b1704497e6498e511e
Piyush Makwana <piyush2k24@gmail.com>
2023-05-05 16:59:46 +0530

    Firedb Project

---
+++ app/src/main/java/com/piyush2k24/firedbcrud/GetStarted/GoogleSignIn.kt
@@ -0,0 +1,12 @@
+package com.piyush2k24.firedbcrud.GetStarted
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.piyush2k24.firedbcrud.R
+
+class GoogleSignIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.google_signin)
+    }
+}
